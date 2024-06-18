45e6d459fe4772d4d7b406823095e52475a39462
tetianaklius <tetyanaklyus@gmail.com>
2024-06-18 17:59:03 +0300

    fixed footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,11 +3,12 @@ import {Outlet} from "react-router-dom";
 
 import HeaderComponent from "../components/HeaderComponent/HeaderComponent";
 import FooterComponent from "../components/FooterComponent/FooterComponent";
+import styles from "./MainLayout.module.css";
 
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className={styles.main_layout}>
             <HeaderComponent/>
             <Outlet/>
             <FooterComponent/>
