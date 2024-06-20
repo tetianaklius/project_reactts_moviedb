5124eb8f68172785a5de068c61b1b9216e51fc6d
tetianaklius <tetyanaklyus@gmail.com>
2024-06-20 19:24:26 +0300

    user info box styling

--- src/components/UserInfoComponent/UserInfoComponent.tsx
+++ src/components/UserInfoComponent/UserInfoComponent.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import styles from "./UserInfoComponent.module.css";
+import user_icon from "../../files/icons8-user-32.png";
 
 const UserInfoComponent = () => {
     return (
         <div className={styles.user_info}>
             <div className={styles.user_icon}>
-                <img src="" alt=""/>
+                <img src={user_icon} alt="user icon" width={"20px"}/>
             </div>
             <div className={styles.user_name}>user name</div>
         </div>
