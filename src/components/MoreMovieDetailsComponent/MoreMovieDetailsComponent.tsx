f49104f20ad6c9f7216a9b00cd5ca60166681ac8
tetianaklius <tetyanaklyus@gmail.com>
2024-06-22 02:30:17 +0300

    code formatting

--- src/components/MoreMovieDetailsComponent/MoreMovieDetailsComponent.tsx
+++ src/components/MoreMovieDetailsComponent/MoreMovieDetailsComponent.tsx
@@ -2,28 +2,19 @@ import React, {FC} from 'react';
 
 import {useAppSelector} from "../../redux/store";
 import styles from "./MoreMovieDetailsComponent.module.css";
-import {useParams} from "react-router-dom";
 
 const MoreMovieDetailsComponent: FC = () => {
     const {movieDetailed} = useAppSelector(state => state.moviesSlice);
-    const {language} = useParams();
-    console.log(language)
+    console.log(movieDetailed?.adult)
     return (
         <div className={styles.more_info_box}>
             <div className={styles.text_box}>
                 <div>бюджет фільму: ${movieDetailed?.budget}</div>
                 <div>країна: {movieDetailed?.origin_country}</div>
                 <div>тривалість: {movieDetailed?.runtime} хв</div>
+                <div>оцінка: {movieDetailed?.vote_average}/10</div>
+                <b>{movieDetailed?.adult && <div>фільм для дорослих: так</div>}</b>
             </div>
-            {/*{language === "uk-UA" ?*/}
-            {/*    <div>бюджет фільму: ${movieDetailed?.budget}</div>*/}
-            {/*    :*/}
-            {/*    <div>movie budget: ${movieDetailed?.budget}</div>*/}
-            {/*}*/}
-            {/*<div> origin country: {movieDetailed?.origin_country}</div>*/}
-            {/*<div> runtime: {movieDetailed?.runtime} min</div>*/}
-
-
         </div>
     );
 };
--- src/services/genres.api.service.ts
+++ src/services/genres.api.service.ts
@@ -2,7 +2,6 @@ import {urls} from "../constants/urls";
 import {IGenres} from "../models/Genres/IGenres";
 import {axiosInstance} from "./api.service";
 
-
 export const genresService = {
     getAll: async (): Promise<IGenres> => {
         const response = await axiosInstance.get<IGenres>(urls.genres.base, {params: {language: "uk-UA"}});
