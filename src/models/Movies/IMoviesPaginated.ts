f326ca14b18f1f954ae29ca380ce42d1b10dea6d
tetianaklius <tetyanaklyus@gmail.com>
2024-06-22 02:32:20 +0300

    edited slice type

--- src/models/Movies/IMoviesPaginated.ts
+++ src/models/Movies/IMoviesPaginated.ts
@@ -1,17 +1,8 @@
 import {IMovie} from "./IMovie";
-import {IMovieDetailed} from "./IMovieDetailed";
 
 export interface IMoviesPaginated {
     page: number,
-    currentPage: number,
     results: IMovie[],
     total_pages?: number
     total_results?: number,
-    isLoaded: boolean,
-    query: string | null,
-    dates: {
-        max: string,
-        min: string
-    },
-    movieDetailed: IMovieDetailed | null;
 }
