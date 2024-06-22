8ce392097ff2e9aff28710b43a192099ac1925c3
tetianaklius <tetyanaklyus@gmail.com>
2024-06-22 14:38:46 +0300

    styling dark and light theme on movie info page

--- src/components/MovieInfoComponent/MovieInfoComponent.tsx
+++ src/components/MovieInfoComponent/MovieInfoComponent.tsx
@@ -4,7 +4,7 @@ import {useLocation} from "react-router-dom";
 import styles from "./MovieInfoComponent.module.css";
 import GenreBadgeComponent from "../GenreBadgeComponent/GenreBadgeComponent";
 import {IMovie} from "../../models/Movies/IMovie";
-import {useAppDispatch} from "../../redux/store";
+import {useAppDispatch, useAppSelector} from "../../redux/store";
 import {moviesActions} from "../../redux/slices/moviesSlice";
 import MoreMovieDetailsComponent from "../MoreMovieDetailsComponent/MoreMovieDetailsComponent";
 import {urls} from "../../constants/urls";
@@ -16,30 +16,39 @@ export const MovieInfoComponent: FC = () => {
 
     const dispatch = useAppDispatch();
     const [trigger, setTrigger] = useState<boolean>(false);
+    const {useDarkTheme} = useAppSelector(state => state.genresSlice);
 
     return (
         <div
             className={styles.movie_component}
-            style={{
-                backgroundImage: `linear-gradient(to right, rgba(255,255,255, 0.65) 0 100%),`
-                    + `url(${urls.poster.base}/${urls.poster.size.original}/${movie.backdrop_path})`,
-                backgroundSize: "cover",
-            }}>
-
+            style={useDarkTheme ?
+                {
+                    backgroundImage: `linear-gradient(to right, rgba(0,0,0, 0.8) 0 100%),`
+                        + `url(${urls.poster.base}/${urls.poster.size.original}/${movie.backdrop_path})`,
+                    backgroundSize: "cover",
+                }
+                :
+                {
+                    backgroundImage: `linear-gradient(to right, rgba(255,255,255, 0.65) 0 100%),`
+                        + `url(${urls.poster.base}/${urls.poster.size.original}/${movie.backdrop_path})`,
+                    backgroundSize: "cover",
+                }
+            }>
             <div className={styles.movie_box}>
-
                 <div className={styles.movie_poster}>
                     <img src={`${urls.poster.base}/${urls.poster.size.size_400}/${movie.poster_path}`}
                          alt="movie poster"/>
                 </div>
-
                 <div className={styles.movie_description}>
                     <div className={styles.title}>
                         {movie.title}
                     </div>
                     <div>
-                        {movie.release_date}
-                        {movie.adult && <small>adult</small>}
+                        <span>{movie.release_date}</span>
+                        {movie.adult &&
+                            <span className={styles.adult_mark}>
+                                <small>для дорослих</small>
+                            </span>}
                     </div>
                     <div className={styles.stars_rating}>
                         <StarsRatingComponent key={movie.id} movie={movie}/>
